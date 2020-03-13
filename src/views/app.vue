3dabfea206cf1800907aa8765a4e3a5555cc106a
huanghaojie <huanghaojie@bingosoft.net>
2020-03-13 17:33:40 +0800

    完成

---
+++ src/views/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="flex:1">
+        <guarantee></guarantee>
+    </div>
+</template>
+
+<!--引入bui-weex样式文件-->
+<style lang="scss" src="bui-weex/src/css/buiweex.scss"></style>
+
+<script>
+    import guarantee from "./guarantee.vue";
+    module.exports = {
+        data () {
+            return {}
+        },
+        components: {
+          guarantee
+        }
+    }
+</script>
---
+++ src/views/guarantee.vue
@@ -0,0 +1,60 @@
+<template>
+  <div ref="wrap">
+      <div class='guarantee' v-bind:style="{'height': $isIPad ? '142wx': '284px'}" @click='openArtificial'>
+        <bui-image src="/image/guarantee.png" radius='8px' width="702px" :height="height" @click='openArtificial'></bui-image>
+      </div>
+  </div>
+</template>
+
+<script>
+  const linkapi = require("linkapi");
+  const dom = weex.requireModule("dom");
+  export default {
+    data() {
+      return {
+        height: '212px',
+        channel: new BroadcastChannel("WidgetsMessage"),
+      }
+    },
+    created() {
+        this.height = this.$isIPad ? '106wx' : '212px'
+    },
+    mounted() {
+        this.broadcastWidgetHeight()
+    },
+    methods: {
+        openArtificial(){
+            linkapi.openOnlineServicer()
+        },
+        getComponentRect(_params) {
+            var that = this
+            dom.getComponentRect(this.$refs.wrap, (ret) => {
+                this.channel.postMessage({
+                    widgetHeight: ret.size.height,
+                    id: _params.id
+                });
+            });
+        },
+        broadcastWidgetHeight() {
+            let _params = this.$getPageParams();
+            // 防止高度通知失败
+            setTimeout(() => {
+                this.getComponentRect(_params)
+            }, 200)
+            setTimeout(() => {
+                this.getComponentRect(_params)
+            }, 1200)
+        }
+    }
+  }
+</script>
+
+<style lang="css" src="../css/common.css"></style>
+<style>
+.guarantee {
+    background: #fff;
+    padding: 18wx 0;
+    flex-direction: row;
+    justify-content: center;
+}
+</style>
